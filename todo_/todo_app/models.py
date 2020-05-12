38365f4a4914c899fb2dd4b1b549fc95de349266
Ajith Sundararaj <simply.ajith@gmail.com>
2020-05-12 18:49:04 +0530

    Add files via upload

--- todo_/todo_app/models.py
+++ todo_/todo_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.db.models.signals import post_save
 # Create your models here.
 class TodoStatus(models.Model):
     status_choices = (
@@ -12,7 +13,6 @@ class TodoStatus(models.Model):
     def __str__(self):
         return self.status
 
-
 class Todo(models.Model):
 
     task_uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
@@ -21,14 +21,25 @@ class Todo(models.Model):
     labels = models.CharField(max_length = 240, blank= True)
     due_date = models.DateField()
     created_date = models.DateTimeField(auto_now=True)
-    is_completed = models.BooleanField(default=False, editable=False)
+    is_completed = models.BooleanField(default=False)
     status = models.ForeignKey(TodoStatus,on_delete= models.CASCADE, related_name="todo_status", null =True)
 
     def __str__(self):
         return self.name
+    
 
 
+def verify_status_completed(sender, **kwargs):
 
+    ins_task_uuid = kwargs['instance'].task_uuid
+    ins_status1 = kwargs['instance'].status
+
+    if str(ins_status1) == 'Completed':
+        Todo.objects.filter(task_uuid =ins_task_uuid).update(is_completed= True)
+    else:
+        Todo.objects.filter(task_uuid =ins_task_uuid).update(is_completed= False)
+
+post_save.connect(verify_status_completed, sender=Todo)
     
 
 
