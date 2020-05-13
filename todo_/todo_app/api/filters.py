714921b4c950493e87ad72d13f5b5d5c96e93c68
Ajith Sundararaj <simply.ajith@gmail.com>
2020-05-13 12:48:27 +0530

    Add files via upload

---
+++ todo_/todo_app/api/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters
+# import django_filters
+from todo_app.models import Todo, TodoStatus
+
+
+class TodoFilter(filters.FilterSet):
+    class Meta:
+        model = Todo
+        fields = {
+            'is_completed': ['exact'],
+            'labels': ['exact'],
+
+        }
